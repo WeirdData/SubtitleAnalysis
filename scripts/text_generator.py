b3d63bbd630018a4668e7534288d4987b4a1b9af
Rohit Suratekar <rohitsuratekar@gmail.com>
2020-10-31 19:53:22 +0100

    New Model with GRU

--- scripts/text_generator.py
+++ scripts/text_generator.py
@@ -5,37 +5,70 @@
 
 import json
 import os
-
+from string import digits, ascii_lowercase
 import numpy as np
-from nltk.tokenize import word_tokenize
+from nltk.tokenize import TweetTokenizer
 from tensorflow import keras
-from scripts.common import get_text, get_text_from_folder
+import tensorflow as tf
 
-REMOVE_WORDS = ["/font", "00FF00", "00FFFF", "00ff00", "00ffff", "01x09",
-                '1x02', '1x04', '1x05', '1x06', '1x07', '1x08', '1x10',
-                "29â\x80\x94", "#", "38b0de"]
+from scripts.common import get_text_from_folder
+
+ALLOWED_CHARS = f"{digits}{ascii_lowercase}?!,."
 
 FILE_ID = "ids.json"
 FILE_LOG = "log.csv"
 FILE_MODEL = "model.h5"
 FILE_TXT = "../input/subtitles/subtext_got.txt"
-SEQ_LENGTH = 7
-TEMPERATURE = 0.5
+SEQ_LENGTH = 15
+TEMPERATURE = 0.6
 BATCH_SIZE = 100
+EMBEDDING = 128
+EPOCHS = 1
+UNITS = 128
 
 
 def get_data():
-    # return get_text("data/bbt", 1)
-    return get_text_from_folder("data/hp")
+    return get_text_from_folder("data/bbt")
     # with open(FILE_TXT) as f:
     #     return f.readlines()[0]
 
 
+def filter_char(word: str):
+    w = list(str(word))
+    w = [x for x in w if x in ALLOWED_CHARS]
+    if len(set(w)) == 1:
+        if list(set(w))[0] == ".":
+            return "."
+    w = "".join(w)
+    if w.startswith("www"):
+        return ""
+    if "x" in w:
+        tmp = w.split("x")
+        try:
+            float(tmp[0])
+            return ""
+        except ValueError:
+            pass
+    try:
+        float(w)
+        return ""
+    except ValueError:
+        pass
+    if len(w) == 6:
+        try:
+            bytes.fromhex(w)
+            return ""
+        except ValueError:
+            pass
+    return w
+
+
 def prepare_data():
+    tk = TweetTokenizer()
     data = get_data()
     data = data.lower()
-    data = word_tokenize(data)
-    data = [x for x in data if x not in REMOVE_WORDS]
+    data = tk.tokenize(data)
+    data = [filter_char(x) for x in data if len(filter_char(x)) > 0]
     vocab = sorted(list(set(data)))
     # Start IDs from 1 so that we can use 0 for dropout
     char2id = {x: i + 1 for i, x in enumerate(vocab)}
@@ -43,51 +76,7 @@ def prepare_data():
     with open(FILE_ID, "w") as f:
         json.dump(char2id, f, sort_keys=True)
     data = [char2id[x] for x in data]
-    seq_data = []
-    for i in range(0, len(data) - SEQ_LENGTH):
-        seq_data.append(data[i:i + SEQ_LENGTH + 1])
-
-    seq_data = np.asarray(seq_data)
-    return seq_data, len(char2id) + 1
-
-
-def get_model(vocab_size, seq_length):
-    model = keras.Sequential([
-        keras.layers.Embedding(input_dim=vocab_size,
-                               output_dim=100,
-                               input_length=seq_length),
-        keras.layers.LSTM(64, return_sequences=True),
-        keras.layers.Dropout(0.2),
-        keras.layers.LSTM(64),
-        keras.layers.Dense(128, activation=keras.activations.relu),
-        keras.layers.Dense(vocab_size, activation=keras.activations.softmax)
-    ])
-    keras.utils.plot_model(model, "model.png", show_shapes=True)
-    return model
-
-
-def train_model():
-    training, vocab_size = prepare_data()
-    model = get_model(vocab_size, SEQ_LENGTH)
-    model.compile(optimizer=keras.optimizers.Adam(),
-                  loss=keras.losses.CategoricalCrossentropy(),
-                  metrics=keras.metrics.Accuracy())
-    try:
-        os.remove(FILE_LOG)
-    except FileNotFoundError:
-        pass
-    csv_logger = keras.callbacks.CSVLogger(FILE_LOG,
-                                           append=True,
-                                           separator=';')
-    model.fit(training[:, :-1],
-              keras.utils.to_categorical(training[:, -1],
-                                         num_classes=vocab_size),
-              epochs=10,
-              shuffle=True,
-              batch_size=BATCH_SIZE,
-              callbacks=[csv_logger])
-
-    model.save(FILE_MODEL)
+    return data, len(char2id) + 1
 
 
 def predict_text(words, no_of_words):
@@ -96,22 +85,71 @@ def predict_text(words, no_of_words):
         ids = json.load(f)
     words = [ids[x] for x in words]
     id2text = {v: k for k, v in ids.items()}
-    model = keras.models.load_model(FILE_MODEL)  # type: keras.Model
+    model = get_model(len(ids) + 1, 1)
+    old_model = keras.models.load_model(FILE_MODEL)
+    model.set_weights(old_model.get_weights())
     current_words = words
+    working_words = [x for x in current_words]
+    while len(working_words) < SEQ_LENGTH:
+        working_words.insert(0, 0)
     for _ in range(no_of_words):
-        predictions = model.predict(np.asarray([current_words]))[0]
-        predictions = np.asarray(predictions).astype('float64')
+        while len(working_words) > SEQ_LENGTH:
+            working_words.pop(0)
+        predictions = model.predict(np.asarray([working_words]))[0]
         predictions = np.log(predictions) / TEMPERATURE
-        exp_pred = np.exp(predictions)
-        preds = exp_pred / np.sum(exp_pred)
-        probas = np.random.multinomial(1, preds, 1)
-        prediction_id = np.argmax(probas)
+        prediction_id = tf.random.categorical(predictions, num_samples=1)[
+            -1, 0].numpy()
         current_words.append(prediction_id)
+        working_words.append(prediction_id)
 
-    final_words = [id2text[x] for x in current_words]
+    final_words = [id2text[x] for x in current_words if x != 0]
     print(" ".join(final_words))
 
 
+def get_model(vocab, batch):
+    model = keras.Sequential()
+    model.add(
+        keras.layers.Embedding(vocab + 1, EMBEDDING, batch_size=batch)
+    )
+    model.add(keras.layers.GRU(UNITS, return_sequences=True, stateful=True,
+                               recurrent_initializer='glorot_uniform'))
+    model.add(keras.layers.GaussianDropout(0.3))
+    model.add(keras.layers.GRU(UNITS, stateful=True, return_sequences=True,
+                               recurrent_initializer='glorot_uniform'))
+    model.add(keras.layers.Dense(EMBEDDING,
+                                 activation=keras.activations.relu))
+    model.add(keras.layers.Dense(vocab + 1,
+                                 activation=keras.activations.softmax))
+    keras.utils.plot_model(model, "model.png", show_shapes=True)
+    return model
+
+
+def train_model():
+    def dense_1_step(batch):
+        # Shift features and labels one step relative to each other.
+        return batch[:-1], batch[1:]
+
+    data, vocab = prepare_data()
+    dataset = tf.data.Dataset.from_tensor_slices(data)
+    dataset = dataset.batch(SEQ_LENGTH + 1, drop_remainder=True).map(
+        dense_1_step)
+    dataset = dataset.shuffle(10000).batch(BATCH_SIZE, drop_remainder=True)
+
+    model = get_model(vocab, BATCH_SIZE)
+    try:
+        os.remove(FILE_LOG)
+    except FileNotFoundError:
+        pass
+    csv_logger = keras.callbacks.CSVLogger(FILE_LOG,
+                                           append=True,
+                                           separator=';')
+    model.compile(optimizer=keras.optimizers.Adam(),
+                  loss=keras.losses.sparse_categorical_crossentropy)
+
+    model.fit(dataset, epochs=EPOCHS, shuffle=False, callbacks=[csv_logger])
+    model.save(FILE_MODEL)
+
+
 def run():
-    # train_model()
-    predict_text(["we", "are"], 20)
+    train_model()
+    # predict_text(["how", "about"], 20)
--- web/src/pages/About.vue
+++ web/src/pages/About.vue
@@ -2,21 +2,14 @@
   <q-page padding class="justify-center q-pa-xl">
     <div class="row justify-center text-justify q-mb-lg">
       <p class="col-xs-12 col-md-5">
-        With the help of this website, you can generate completely novel dialogues from your favourite
-        serial with the help of Artificial Intelligence. You only have to provide the starting words and
-        my neural network will complete the rest of the dialogue. Currently, these results are far from perfect
-        but enjoy the art :) <br/>
-        This project is part of bigger project called <a href="https://github.com/WeirdData/SubtitleAnalysis">SubtitleAnalysis</a>.
-        It deals with various aspect of NLP and Text Generation algorithms. All neural networks used on this website
-        were first trained on subtitles of following serials in python. Those models were then imported here
-        to create the user interface. More technical details will be available on my Data-Science <a
-        href="https://weirddata.github.io/" target="_blank">blog</a>.
-        Following are some of the important points regarding these models,
+        Simple idea is to make a platform where one can generate artificial screenplay for the entire TV serial. Where the
+        user can tell how they want the dialogue to start and Artificial Intelligence model will complete the rest of the conversation. It will
+        provide enough space for both creativity as well as <a href="https://en.wikipedia.org/wiki/Natural_language_processing" target="_blank">NLP</a> implementation. Currently, these results are far from perfect
+        but enjoy the art :) For more information, please check my Data-Science <a
+        href="https://weirddata.github.io/2020/10/16/text-generation.html" target="_blank">blog</a>.
+        Following are some of the important points regarding NLP models used in this website,
         <q-list separator dense>
           <q-item></q-item>
-          <q-item>Due to limitations in computational power, only a few 'epoch' were used for training of neural
-            network.
-          </q-item>
           <q-item>Game of Thrones model was only trained on subtitles from season 1</q-item>
           <q-item>Big Bang Theory, Criminal Minds, Breaking Bad models were trained on first 250000 characters</q-item>
           <q-item>Harry Potter series model includes subtitles from all 7 Harry Potter movies</q-item>
@@ -26,8 +19,9 @@
 
         All the home page posters were taken from IMDB. Website was designed by Rohit Suratekar. Full
         source-code can be found <a href="https://github.com/WeirdData/SubtitleAnalysis/tree/master/web"
-                                    target="_blank"> here</a>. If you have an idea about this project, do
-        share with me. You can contact me on <b>rohitsuratekar@protonmail.com</b>
+                                    target="_blank"> here</a>. If you have an idea regarding this project, or
+        want me to add your favorite TV serial here, do
+        contact with me. You can reach me on <b>rohitsuratekar@protonmail.com</b>
 
       </p>
     </div>
